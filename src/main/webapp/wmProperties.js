e213dd395e3c56801679ab8965d799df51bab507
root <root@2d0e2de3795c>
2016-07-13 03:59:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Test_share1_12juLY",
+  "homePage" : "Main",
+  "name" : "Test_share1_12juLY",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
